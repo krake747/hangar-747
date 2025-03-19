a6870b7ce27464e208ebb0c2736dac1794ef677a
Kevin Kraemer <kraemer.kevin747@gmail.com>
2025-03-19 20:11:17 +0100

    feat(apps): Using vite, react ts and styled components

--- apps/restyled/src/App.tsx
+++ apps/restyled/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from "styled-components";
+import "./App.css";
+import Button from "./ui/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Grid>
+            <Button size="small" variant="fill">
+                Button
+            </Button>
+            <Button size="small" variant="outline">
+                Button
+            </Button>
+            <Button size="small" variant="ghost">
+                Button
+            </Button>
+            <Button size="medium" variant="fill">
+                Button
+            </Button>
+            <Button size="medium" variant="outline">
+                Button
+            </Button>
+            <Button size="medium" variant="ghost">
+                Button
+            </Button>
+            <Button size="large" variant="fill">
+                Button
+            </Button>
+            <Button size="large" variant="outline">
+                Button
+            </Button>
+            <Button size="large" variant="ghost">
+                Button
+            </Button>
+        </Grid>
+    );
 }
 
-export default App
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 180px);
+    gap: 64px;
+    padding: 48px;
+    place-items: center;
+`;
+
+export default App;

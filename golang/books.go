145746b8ef959af1c6e8d9f6109bf2291f4aa84e
Kevin Kraemer <kraemer.kevin747@gmail.com>
2025-12-08 16:22:42 +0100

    learn more about maps and concurrent testing

--- golang/books.go
+++ golang/books.go
@@ -13,21 +13,28 @@ type Book struct {
 	Copies int
 }
 
-var catalog = map[string]Book{
-	"abc": {ID: "abc", Title: "In the Company of Cheerful Ladies", Author: "Alexander McCall Smith", Copies: 1},
-	"def": {ID: "def", Title: "White Heat", Author: "Dominic Sandbrook", Copies: 2},
+func GetCatalog() map[string]Book {
+	catalog := map[string]Book{
+		"abc": {ID: "abc", Title: "In the Company of Cheerful Ladies", Author: "Alexander McCall Smith", Copies: 1},
+		"def": {ID: "def", Title: "White Heat", Author: "Dominic Sandbrook", Copies: 2},
+	}
+	return catalog
 }
 
 func BookToString(b Book) string {
 	return fmt.Sprintf("%v by %v (copies: %v)", b.Title, b.Author, b.Copies)
 }
 
-func GetAllBooks() []Book {
+func GetAllBooks(catalog map[string]Book) []Book {
 	books := maps.Values(catalog)
 	return slices.Collect(books)
 }
 
-func GetBook(ID string) (Book, bool) {
+func GetBook(catalog map[string]Book, ID string) (Book, bool) {
 	book, ok := catalog[ID]
 	return book, ok
+}
+
+func AddBook(catalog map[string]Book, b Book) {
+	catalog[b.ID] = b
 }
--- golang/books_test.go
+++ golang/books_test.go
@@ -2,9 +2,17 @@ package books_test
 
 import (
 	"books"
+	"cmp"
 	"slices"
 	"testing"
 )
+
+func getTestCatalog() map[string]books.Book {
+	return map[string]books.Book{
+		"abc": {ID: "abc", Title: "In the Company of Cheerful Ladies", Author: "Alexander McCall Smith", Copies: 1},
+		"def": {ID: "def", Title: "White Heat", Author: "Dominic Sandbrook", Copies: 2},
+	}
+}
 
 func TestBookToString_FormatsBookInfoAsString(t *testing.T) {
 	t.Parallel()
@@ -26,7 +34,10 @@ func TestGetAllBooks_ReturnsAllBooks(t *testing.T) {
 		{ID: "abc", Title: "In the Company of Cheerful Ladies", Author: "Alexander McCall Smith", Copies: 1},
 		{ID: "def", Title: "White Heat", Author: "Dominic Sandbrook", Copies: 2},
 	}
-	got := books.GetAllBooks()
+	got := books.GetAllBooks(getTestCatalog())
+	slices.SortFunc(got, func(a, b books.Book) int {
+		return cmp.Compare(a.Author, b.Author)
+	})
 	if !slices.Equal(want, got) {
 		t.Fatalf("want %v, got %v", want, got)
 	}
@@ -35,7 +46,7 @@ func TestGetAllBooks_ReturnsAllBooks(t *testing.T) {
 func TestGetBook_FindsBookInCatalogByID(t *testing.T) {
 	t.Parallel()
 	want := books.Book{ID: "abc", Title: "In the Company of Cheerful Ladies", Author: "Alexander McCall Smith", Copies: 1}
-	got, ok := books.GetBook("abc")
+	got, ok := books.GetBook(getTestCatalog(), "abc")
 	if !ok {
 		t.Fatalf("book not found")
 	}
@@ -46,8 +57,27 @@ func TestGetBook_FindsBookInCatalogByID(t *testing.T) {
 
 func TestGetBook_ReturnsFalseWhenBookNotFound(t *testing.T) {
 	t.Parallel()
-	_, ok := books.GetBook("nonexistent ID")
+	_, ok := books.GetBook(getTestCatalog(), "nonexistent ID")
 	if ok {
 		t.Fatalf("want false for nonexistent book ID, got true")
+	}
+}
+
+func TestAddBook_AddsGiveBookToCatalog(t *testing.T) {
+	t.Parallel()
+	catalog := getTestCatalog()
+	_, ok := books.GetBook(catalog, "123")
+	if ok {
+		t.Fatalf("book already present")
+	}
+	books.AddBook(catalog, books.Book{
+		ID:     "123",
+		Title:  "The Prize of all the Oceans",
+		Author: "Glyn Williams",
+		Copies: 2,
+	})
+	_, ok = books.GetBook(catalog, "123")
+	if !ok {
+		t.Fatalf("added book not found")
 	}
 }

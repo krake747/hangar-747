340ac1ebd8e8a27b2e936c4486cb87240b06085e
Kevin Kraemer <kraemer.kevin747@gmail.com>
2025-12-09 18:27:32 +0100

    add server and client programs

---
+++ golang/cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.ListenAndServe(":3000", http.HandlerFunc(hello))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}

c99d67f1a77b2a915f722bb24c2c29d11f2e3523
Kevin Kraemer <kraemer.kevin747@gmail.com>
2025-12-21 15:11:41 +0100

    feat: add endpoint to communicate with skyhelp

--- dotnet/SkyOps/Program.cs
+++ dotnet/SkyOps/Program.cs
@@ -6,7 +6,24 @@ var app = builder.Build();
 
 app.MapGet(
     "/",
-    static () => Results.Ok(new { Message = "SkyOps! How may we help you?" })
+    static () => Results.Ok(
+        new
+        {
+            Message = "SkyOps! How may we help you?",
+            RedirectToSkyHelp = "/redirect/skyhelp"
+        }
+    )
+);
+
+app.MapGet(
+    "/redirect/skyhelp",
+    async static (HttpClient httpClient, IConfiguration config) =>
+    {
+        var skyHelpUrl = config["Networking:SkyHelp"] ?? "http://skyhelp:8080/";
+        var response = await httpClient.GetAsync(skyHelpUrl);
+        var content = await response.Content.ReadAsStringAsync();
+        return Results.Content(content, "application/json");
+    }
 );
 
 app.Run();
